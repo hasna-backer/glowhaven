12d217e5c4bdca094d61e5a5dce85122d469f059
Hasna Aabi <hasnaaabi@Hasnas-MacBook-Air.local>
2024-05-22 13:49:28 +0530

    user cleanup done

--- utils/automate.js
+++ utils/automate.js
@@ -17,14 +17,14 @@
 
 //     const page = await browser.newPage();
 
-//     await page.goto(`http://localhost:4011`);
+//     await page.goto(`http://localhost:4000`);
 
 //     const userNameInput = await page.waitForSelector('#your_name');
 //     await userNameInput.type('hasnaashik3@gmail.com');
 
 //     const passwordInput = await page.waitForSelector('#your_pass');
 //     await passwordInput.type('12345678');
-
+ 
 //     const loginBtn = await page.waitForSelector('#signin');
 //     await loginBtn.click();
 // };
